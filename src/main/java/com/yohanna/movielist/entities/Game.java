c4f98d918a7ada4ac3eb2cb258170d247a4c5bbc
YohannaOliveira <yohannaoliveira50@gmail.com>
2025-01-25 01:38:45 -0300

    feat: game seed

--- src/main/java/com/yohanna/movielist/entities/Game.java
+++ src/main/java/com/yohanna/movielist/entities/Game.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @EqualsAndHashCode
-@Table(name = "tb_name")
+@Table(name = "tb_game")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +21,8 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 }
