dac6bc06bed7e9e68612db14ed9ebac94259f967
YohannaOliveira <yohannaoliveira50@gmail.com>
2025-01-25 01:28:57 -0300

    feat: class created and configurations

---
+++ src/main/java/com/yohanna/movielist/entities/Game.java
@@ -0,0 +1,26 @@
+package com.yohanna.movielist.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+@Table(name = "tb_name")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
